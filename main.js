daf3603c6c67da897b199872d6c179b24fa1a9ad
Atef Fawzy <ateffawzy897@gmail.com>
2025-02-11 16:24:58 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let li = document.querySelectorAll(" li ");
+//======================================
+//  Add And  Remove All Class List
+li.forEach((item) => {
+  item.addEventListener("mouseenter", function () {
+    li.forEach((li) => li.classList.remove("active"));
+    this.classList.add("active");
+  });
+
+  item.addEventListener("mouseleave", function () {
+    this.classList.remove("active");
+  });
+});
+//===========================================
+li.forEach((e) => {
+  e.addEventListener("click", function () {
+    const link = this.querySelector("a");
+    if (link) {
+      link.click();
+    }
+  });
+});
+
